b870538a7854bcc26edc8196b75adf4863332f61
Vinicius Correa <viniciuscorrea91@gmail.com>
2014-11-12 19:16:30 -0200

    Released version 0.8.2-debug

--- TaqtileSDK/Headers/TQGeotrigger.h
+++ TaqtileSDK/Headers/TQGeotrigger.h
@@ -9,11 +9,40 @@
 #import <Foundation/Foundation.h>
 #import <GeotriggerSDK/GeotriggerSDK.h>
 
+/** TQGeotrigger
+ *  Simply commands when to start and when to stop the geotrigger service
+ */
 @interface TQGeotrigger : NSObject
-
+/** shared
+ *  Return an instance of itself, initializating it if there is no TQGeotrigger object. Since it's an shared object, it'll only create one.
+ *
+ * @return a TQGeotrigger object
+ */
 +(TQGeotrigger *) shared;
 
+/** startGeotriggerService clientId withTags
+ *  Starts the geotrigger service
+ *
+ * @param clientID: the client ID
+ * @param tags: An optional list of tags to be set on the device after it registers itself
+ * @see https://developers.arcgis.com/geotrigger-service/ios-reference/Classes/AGSGTGeotriggerManager.html#//api/name/setupWithClientId:trackingProfile:registerForRemoteNotifications:isProduction:tags:completion:
+ */
 -(void)startGeotriggerService:(NSString *)clientId withTags:(NSArray *)tags;
+
+/** stopGeotriggerService clientId withTags
+ *  Stops the geotrigger service
+ *
+ * @param clientID: the client ID
+ * @param tags: An optional list of tags to be set on the device after it stops
+ */
 -(void)stopGeotriggerService:(NSString *)clientId withTags:(NSArray *)tags;
+
+
+/** trackingId
+ *  gets the registered track id to the device
+ *
+ * @return a string representing the track id
+ */
++(NSString *)trackingId;
 
 @end
